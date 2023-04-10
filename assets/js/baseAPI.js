4cb32c05e45e5a17a14974e6d82c11430c71c831
zhushushu <zss5527@163.com>
2023-04-10 17:01:45 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -15,8 +15,8 @@ $.ajaxPrefilter(function(options){
 
     // 全统一挂载complete回调函数，
     options.complete = function(res){
-        console.log('执行了complete回调函数');
-        console.log(res);
+        // console.log('执行了complete回调函数');
+        // console.log(res);
         // 在complete回调函数中可以使用res.responseJSON拿到服务器响应回来的数据
         // 
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
